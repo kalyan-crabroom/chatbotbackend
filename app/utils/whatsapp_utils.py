830db5a8dbe50a6bf43731f0f6db240940483b09
kalyan-crabroom <kalyan@crabroom.com>
2024-06-08 18:51:14 -0400

    openai

--- app/utils/whatsapp_utils.py
+++ app/utils/whatsapp_utils.py
@@ -84,7 +84,7 @@ def process_whatsapp_message(body):
     message_body = message["text"]["body"]
 
     # TODO: implement custom function here
-    response = generate_response(message_body)
+    # response = generate_response(message_body)
 
     # OpenAI Integration
     response = generate_response(message_body, wa_id, name)
